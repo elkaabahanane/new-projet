868c87b3616fde9855f070f1930f62ddcc939131
unknown <hanane.elkaaba1@gmail.com>
2020-04-01 18:07:11 +0100

    add back-end

---
+++ Brief-6/public/js/Inscription.js
@@ -0,0 +1,23 @@
+// $(document).ready(function () {
+//     $("#email").keyup(function () {
+//         if (validateEmail()) {
+//             $("#email").css("border", "2px solid green");
+//         } else {
+//             $("#email").css("border", "border", "2px solid red");
+//             // $("emailMsg").html("<p class='text-danger'>un-Valid</p>")
+//         }
+
+//     });
+
+// });
+
+// function validateEmail() {
+//     var email = $("#email").val();
+//     var reg = /^w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)"$/
+//     if (reg.test(email)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
